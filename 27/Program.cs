6adc3d76e809e5c2728f671fd2f0da0b339a622e
Olga <olga252318@mail.ru>
2023-06-03 21:39:57 +0300

    27 papka

---
+++ 27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+int date = Number("Введите число: ");
+Console.WriteLine($"Сумма цифр {date} = {Sumnum(date)}");
+
+int Number(string message)
+{
+Console.WriteLine("Введите число:");
+string a = Console.ReadLine();
+int result = int.Parse(a);
+return result;
+}
+
+int Sumnum(int n)
+{
+    int sum = 0;
+    while(n > 0)
+    {
+        sum += n % 10;
+        n = n / 10;
+    }
+    return sum;
+}
